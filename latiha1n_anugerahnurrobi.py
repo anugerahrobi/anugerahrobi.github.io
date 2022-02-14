21b20ef0a60548403d50c72a0ab406fed9e51438
Anonymusi <67617270+anugerahrobi@users.noreply.github.com>
2022-02-14 20:01:17 +0700

    Create latiha1n_anugerahnurrobi.py

---
+++ latiha1n_anugerahnurrobi.py
@@ -0,0 +1,21 @@
+#JAWAB TUGAS 1
+
+nama = "Muhammad Anugerah Nurrobi"
+umur = 24
+kota = "Palembang"
+
+#jawaban soal 1
+#untuk cetak nama nya karena string
+print(nama)
+#pakai str karena isi variabel umur adalah interger
+print(str(umur)+"tahun")
+#string
+print(kota)
+
+#jawaban soal 2
+print("Nama saya " + nama + ",umur saya " + str(umur)+"tahun, " + "tinggal di " + kota)
+
+#jawaban soal 3
+kalimat = 'Budi menyukai buah mangga'
+kalimat_2 = kalimat.replace('mangga','apel')
+print(kalimat_2)
